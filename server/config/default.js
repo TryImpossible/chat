79f9a1b3de5322cf2c2da7985d84f7c08f2df73b
Barry <865068275@qq.com>
2020-06-25 16:52:31 +0800

    格式化代码

---
+++ server/config/default.js
@@ -0,0 +1,14 @@
+const config = {
+  // 启动端口
+  port: 3001,
+  // 数据库配置
+  database: {
+    DATABASE: 'chat',
+    USERNAME: 'root',
+    PASSWORD: 'password',
+    PROT: '3306',
+    HOST: 'localhost',
+  },
+};
+
+module.exports = config;
